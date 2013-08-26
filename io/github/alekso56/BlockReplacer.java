e03dd216d68cc6066dd252350ef9389b662f52af
alekso56 <simple_and_easy@hotmail.com>
2013-08-26 19:25:27 +0200

    Ready for first release.

--- io/github/alekso56/BlockReplacer.java
+++ io/github/alekso56/BlockReplacer.java
@@ -34,7 +34,6 @@ import org.bukkit.util.Vector;
 
 public final class BlockReplacer extends JavaPlugin implements Listener{
 	public static int id;
-	public World world;
 	ArrayList<String> Proclist = new ArrayList<String>();
 	public static Map<String, String> ST = new HashMap<String, String>(); // selected tool
 	public void onEnable(){
@@ -49,7 +48,8 @@ public final class BlockReplacer extends JavaPlugin implements Listener{
 	    public void run()
 	    {
 	        for(int x = 1; x < Proclist.size(); x = x+1){
-	    	  splitString(Proclist.get(x),x);}
+	    	  splitString(Proclist.get(x),x);
+	    	  }
 	        }
 	    },getConfig().getInt("Launch.NextBlockScan") * 20, getConfig().getInt("Launch.NextBlockScan") * 20) > 0) 
 	    {
@@ -69,7 +69,6 @@ public final class BlockReplacer extends JavaPlugin implements Listener{
 			sender.sendMessage("here's your spam!");
 			return true;
 		}
-		//else
 		return false; 
 	}
 	 public void saveArray(ArrayList<String> proclist2) {
@@ -94,11 +93,14 @@ public final class BlockReplacer extends JavaPlugin implements Listener{
 			  int z = Integer.parseInt(stringtokenizer.nextToken());
 			  int Material = Integer.parseInt(stringtokenizer.nextToken());
 			  int timeStamps = Integer.parseInt(stringtokenizer.nextToken());
-	          //if timestamp - currenttime  = bigger than config value then (needs fixing)
+	          //if timestamp - currenttime  = bigger than config value then
 			  int timeStamp = Integer.parseInt(cTime());
-			  if(timeStamps - timeStamp > getConfig().getInt("Launch.CheckTime")){
-				  world.getBlockAt(x,y,z).setTypeId(Material);
+			  int result = timeStamp - timeStamps;
+			  getLogger().info("Scanning "+index+" result:"+result+" TimeCheck: "+getConfig().getInt("Launch.CheckTime"));
+			  if(result > getConfig().getInt("Launch.CheckTime")){
 				  Proclist.remove(index);
+				  World world= getServer().getWorld("world");
+				  world.getBlockAt(x, y, z).setTypeId(Material, true);
 				  getLogger().info("set "+x+" "+y+" "+z+" "+Material+" And deleted: "+index);
 			  }
 	  }
@@ -135,36 +137,37 @@ public ArrayList<String> loadArray() {
     public void onBlockBreak(BlockBreakEvent event)
     {
         Block b = event.getBlock();
-        org.bukkit.Material b1 = b.getType();
+        Material b1 = b.getType();
         int timeStamp = Integer.parseInt(cTime());
         String dbString =  joinString(b.getX(),b.getY(),b.getZ(),b.getTypeId(),timeStamp);
-        if (world == null){Player player = event.getPlayer();
-    	world = player.getWorld();}
-        if (b1 == Material.LOG && ST.get(event.getPlayer().getName()) == "AXE")
+        if (b1 == Material.LOG && ST.get(event.getPlayer().getName()) == "AXE" && !getConfig().getBoolean("Blocks.LOGS"))
         {
-        	b.setType(Material.WOOD);
+        	b.setTypeId(5);
         	event.getPlayer().getInventory().addItem(new ItemStack(Material.LOG, 1));
         	AddToDb(dbString);
         	event.setCancelled(true);
         }
-        else if (b1 == Material.LEAVES && ST.get(event.getPlayer().getName()) == "SHEARS")
+        else if (b1 == Material.LEAVES && !getConfig().getBoolean("Blocks.LEAVES") && ST.get(event.getPlayer().getName()) == "SHEARS")
         {
         	AddToDb(dbString);
         }
-        else if (b1 == Material.IRON_ORE ||b1 == Material.GOLD_ORE ||b1 == Material.DIAMOND_ORE ||b1 == Material.LAPIS_ORE ||b1 == Material.COAL_ORE && ST.get(event.getPlayer().getName()) == "PICKAXE"){
-        	if(b1 == Material.IRON_ORE){b.setType(Material.WOOL); b.setData((byte)8); event.getPlayer().getInventory().addItem(new ItemStack(Material.IRON_ORE, 1));}
-            else if(b1 == Material.COAL_ORE){b.setType(Material.WOOL); b.setData((byte)15);event.getPlayer().getInventory().addItem(new ItemStack(Material.COAL_ORE, 1));}
-            else if(b1 == Material.GOLD_ORE){b.setType(Material.WOOL); b.setData((byte)4);event.getPlayer().getInventory().addItem(new ItemStack(Material.GOLD_ORE, 1));}
-            else if(b1 == Material.DIAMOND_ORE){b.setType(Material.WOOL); b.setData((byte)9);event.getPlayer().getInventory().addItem(new ItemStack(Material.DIAMOND, 1));}
-            else if(b1 == Material.LAPIS_ORE){b.setType(Material.WOOL); b.setData((byte)11);short itemDamage = 4;event.getPlayer().getInventory().addItem(new ItemStack(Material.INK_SACK, 4 , itemDamage));}
-            else if(b1 == Material.STONE){b.setTypeId(4);event.getPlayer().getInventory().addItem(new ItemStack(Material.STONE, 1));}
+        else if (b1 == Material.IRON_ORE ||b1 == Material.GOLD_ORE ||b1 == Material.DIAMOND_ORE ||b1 == Material.LAPIS_ORE ||b1 == Material.COAL_ORE||b1 == Material.STONE||b1 == Material.EMERALD_ORE ||b1 == Material.GLOWING_REDSTONE_ORE || b1 == Material.REDSTONE_ORE && ST.get(event.getPlayer().getName()) == "PICKAXE"){
+        	if(b1 == Material.IRON_ORE&& !getConfig().getBoolean("Blocks.IRON")){b.setType(Material.WOOL); b.setData((byte)8); event.getPlayer().getInventory().addItem(new ItemStack(Material.IRON_ORE, 1));}
+            else if(b1 == Material.COAL_ORE && !getConfig().getBoolean("Blocks.COAL")){b.setType(Material.WOOL); b.setData((byte)15);event.getPlayer().getInventory().addItem(new ItemStack(Material.COAL, 1)); event.getPlayer().giveExp(1);}
+            else if(b1 == Material.GOLD_ORE && !getConfig().getBoolean("Blocks.GOLD")){b.setType(Material.WOOL); b.setData((byte)4);event.getPlayer().getInventory().addItem(new ItemStack(Material.GOLD_ORE, 1));}
+            else if(b1 == Material.DIAMOND_ORE && !getConfig().getBoolean("Blocks.DIAMOND")){b.setType(Material.WOOL); b.setData((byte)9);event.getPlayer().getInventory().addItem(new ItemStack(Material.DIAMOND, 1));  event.getPlayer().giveExp(5);}
+            else if(b1 == Material.GLOWING_REDSTONE_ORE || b1 == Material.REDSTONE_ORE && !getConfig().getBoolean("Blocks.REDSTONE")){b.setType(Material.WOOL); b.setData((byte)14);event.getPlayer().getInventory().addItem(new ItemStack(Material.REDSTONE, 4)); event.getPlayer().giveExp(3);}
+            else if(b1 == Material.EMERALD_ORE&& !getConfig().getBoolean("Blocks.EMERALD")){b.setType(Material.WOOL); b.setData((byte)5);event.getPlayer().getInventory().addItem(new ItemStack(Material.EMERALD, 1)); event.getPlayer().giveExp(6);}
+            else if(b1 == Material.LAPIS_ORE&& !getConfig().getBoolean("Blocks.LAPIS")){b.setType(Material.WOOL); b.setData((byte)11);short itemDamage = 4;event.getPlayer().getInventory().addItem(new ItemStack(Material.INK_SACK, 4 , itemDamage)); event.getPlayer().giveExp(3);}
+            else if(b1 == Material.STONE&& !getConfig().getBoolean("Blocks.STONE")){b.setType(Material.COBBLESTONE);event.getPlayer().getInventory().addItem(new ItemStack(Material.COBBLESTONE, 1));}
         	event.setCancelled(true);
         	AddToDb(dbString);
         }
-        else if (b1 == Material.SAND || b1 == Material.CLAY && ST.get(event.getPlayer().getName()) == "SPADE")
+        else if (b1 == Material.SAND || b1 == Material.CLAY || b1 == Material.GRAVEL && ST.get(event.getPlayer().getName()) == "SPADE")
         {
-        	if(b1 == Material.SAND){b.setType(Material.SANDSTONE);event.getPlayer().getInventory().addItem(new ItemStack(Material.SAND, 1));}
-        	else if(b1 == Material.CLAY){b.setType(Material.WOOL); b.setData((byte)7);event.getPlayer().getInventory().addItem(new ItemStack(337, 4));}
+        	if(b1 == Material.SAND&& !getConfig().getBoolean("Blocks.SAND")){b.setType(Material.SANDSTONE);event.getPlayer().getInventory().addItem(new ItemStack(Material.SAND, 1));}
+        	else if(b1 == Material.CLAY&& !getConfig().getBoolean("Blocks.CLAY")){b.setType(Material.WOOL); b.setData((byte)7);event.getPlayer().getInventory().addItem(new ItemStack(337, 4));}
+        	else if(b1 == Material.GRAVEL&& !getConfig().getBoolean("Blocks.GRAVEL")){b.setType(Material.STAINED_CLAY); b.setData((byte)9);event.getPlayer().getInventory().addItem(new ItemStack(Material.GRAVEL, 1));}
         	event.setCancelled(true);
         	AddToDb(dbString);
         }
@@ -177,13 +180,13 @@ public ArrayList<String> loadArray() {
         ItemStack i = p.getInventory().getItem(event.getNewSlot());
         if(i == null){id = 0;}
         else{id = i.getTypeId();}
-		getLogger().info(id + " is the typeID!");
-        getLogger().info(event.getNewSlot() + " is the itemslot!");
-        if(id == 269 || id == 284 || id == 273 || id == 277 ){p.sendMessage(p.getName() + " equipped A SPADEEEE!!!"); ST.put(p.getName(), "SPADE");}
-        else if(id == 270 || id == 285 || id == 274 || id == 278){p.sendMessage(p.getName() + " equipped An PICKAXEe!!!"); ST.put(p.getName(), "PICKAXE");}
-        else if(id == 271 || id == 286 || id == 279 || id == 275){p.sendMessage(p.getName() + " equipped An AXE!!!"); ST.put(p.getName(), "AXE");}
-        else if(id == 268 || id == 267 || id == 272 || id == 276){p.sendMessage(p.getName() + " equipped A sword!!!"); ST.put(p.getName(), "SWORD");}
-        else if(id == 359){p.sendMessage(p.getName() + " equipped shears!"); ST.put(p.getName(), "SHEARS");}
+		//getLogger().info(id + " is the typeID!");
+        //getLogger().info(event.getNewSlot() + " is the itemslot!");
+        if(id == 269 || id == 284 || id == 273 || id == 277||id == 256){ST.put(p.getName(), "SPADE");}
+        else if(id == 257 || id == 274 || id == 270 || id == 285 ||id == 278){ST.put(p.getName(), "PICKAXE");}
+        else if(id == 271 || id == 286 || id == 279 || id == 275 ||id == 258){ ST.put(p.getName(), "AXE");}
+        else if(id == 268 || id == 267 || id == 272 || id == 276||id == 283){ST.put(p.getName(), "SWORD");}
+        else if(id == 359){ST.put(p.getName(), "SHEARS");}
         else{ST.put(p.getName(), "NONE");} //player has no tool
                 }
     
@@ -191,7 +194,6 @@ public ArrayList<String> loadArray() {
     	Calendar cal = Calendar.getInstance();
     	SimpleDateFormat sdf = new SimpleDateFormat("hhmmss");
 		String currentTime = sdf.format(cal.getTime());
-    	System.out.println(currentTime);
 		return currentTime;
     }
 	
@@ -206,5 +208,6 @@ public ArrayList<String> loadArray() {
              send.setVelocity(dir.multiply(8));
              send.setFallDistance(-150.0F);
            }
+    	 if(event.getPlayer().getGameMode() != GameMode.CREATIVE){event.setCancelled(true);}
      }
     }
